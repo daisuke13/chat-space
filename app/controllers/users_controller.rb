5c50a002b3a4b9db3839c88e5380172808ff18ac
daisuke13 <people.in.the.box.13@gmail.com>
2017-05-04 20:04:54 +0900

    add editing function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,15 @@
 class UsersController < ApplicationController
-  def edit
-    binding.pry
-    @user = User.find(params[:id])
-  end
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
 
-  def update
-    user = User.find(params[:id])
-    user.update(user_params)
-  end
+  # def update
+  #   user = User.find(params[:id])
+  #   user.update(user_params)
+  # end
 
-  private
-  def user_params
-    params.permit(:name)
-  end
+  # private
+  # def user_params
+  #   params.permit(:name)
+  # end
 end
