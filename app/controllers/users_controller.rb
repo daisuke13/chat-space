1ccffe3ac1aed96779fabbe16acbe9b588ee728d
daisuke13 <people.in.the.box.13@gmail.com>
2017-05-09 16:19:43 +0900

    add alert

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,9 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     if user.update(user_params)
       redirect_to :root, notice: '編集されました'
+    else
+      redirect_to :edit_user_path, alert: 'エラーが発生しました'
+    end
   end
 
   private
