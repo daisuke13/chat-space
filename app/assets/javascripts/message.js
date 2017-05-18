ecfa517ad1609293021e36e5f7b08bec0b1f7304
daisuke13 <people.in.the.box.13@gmail.com>
2017-05-18 12:53:34 +0900

    微修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -19,6 +19,14 @@ $(function() {
     var html =
       `<p class="alert alert-notice">メッセージを送信しました</p>`
     $('.alert').append(html);
+    $('.alert-notice').fadeIn(500).fadeOut(2000);
+  }
+
+  function scroll() {
+    var scroll =
+      $('.chat-main__body').delay(100).animate({
+        scrollTop: $('.chat-main__body')[0].scrollHeight
+      }, 1500);
   }
 
   $('#new_message').on('submit', function(e) {
@@ -40,12 +48,9 @@ $(function() {
       $('.chat-main__body--messages-list').append(html);
       textField.val('');
       $("input").prop("disabled", false)
-      $('.chat-main__body').delay(100).animate({
-          scrollTop: $('.chat-main__body')[0].scrollHeight
-      }, 1500);
 
       flash();
-      $(".alert-notice").fadeIn(1000).fadeOut(3000);
+      scroll();
     })
 
     .fail(function() {
