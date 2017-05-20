f43e4f1fe97e3d19ad4ae45f163213a766e0a836
daisuke13 <people.in.the.box.13@gmail.com>
2017-05-20 13:59:37 +0900

    微修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation, :current_password)
   end
